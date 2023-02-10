d1e54c22656ddc6e8ed0c427e4872c55df789a2c
aasurak <atharvashelke12345@gmail.com>
2023-02-10 16:12:57 +0530

    Added my project

--- a.js
+++ a.js
@@ -1,8 +1,7 @@
 console.log("dd");
 console.dir(document);
-var items = document.getElementsByClassName("list-group-item");
-items[1].textContent = "A";
-var l1 = document.getElementsByClassName("l1");
-items[2].style.backgroundColor = "green";
-for (var i =0;i<li.length;i++){
-    li[i].style.fontWeight = "bold";}
+var items = document.getElementsByClassName("list-group-item-new");
+console.log(items[0].innerHTML);
+var items = document.getElementById("items");
+var newItem = items.getElementsByTagName("li")[4];
+console.log(newItem.innerHTML);
