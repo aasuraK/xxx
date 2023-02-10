74178fb75b72ba5dc0078016dca2408e9632d3e3
aasurak <atharvashelke12345@gmail.com>
2023-02-10 20:43:39 +0530

    noe

--- a.js
+++ a.js
@@ -1,7 +1,25 @@
-console.log("dd");
-console.dir(document);
-var items = document.getElementsByClassName("list-group-item-new");
-console.log(items[0].innerHTML);
-var items = document.getElementById("items");
-var newItem = items.getElementsByTagName("li")[4];
-console.log(newItem.innerHTML);
+console.log("hey");
+//console.dir(document);
+//document.title = 123;
+console.log(document.doctype);
+console.log(document.getElementById("header-title"));
+let headerTitle = document.getElementById('header-title');
+console.log(headerTitle);
+headerTitle.innerHTML = '<h3>Enjoy</h3>';
+// get element by class name
+let b = document.getElementsByClassName('list-group-item');
+console.log(b);
+b[1].textContent = "2nd";
+let a = document.querySelector('.list-group-item:nth-child(2)');
+a.style.backgroundColor = "green";
+let submit = document.querySelector("input[type='submit']");
+submit.value = "SEND"
+//let thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+//thirdItem.style.display = "none";
+var c = document.querySelectorAll('.list-group-item');
+c[1].style.color = "lightgreen";
+var odd = document.querySelectorAll("li:nth-child(odd)");
+for(var i =0; i<odd.length;i++){
+    odd[i].style.backgroundColor = "grey";
+
+}
