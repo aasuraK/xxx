aa00915a673604ada09f32ae3d0a4692718c3743
aasurak <atharvashelke12345@gmail.com>
2023-02-10 15:23:24 +0530

    first commit

---
+++ a.js
@@ -0,0 +1,8 @@
+console.log("dd");
+console.dir(document);
+var items = document.getElementsByClassName("list-group-item");
+items[1].textContent = "A";
+var l1 = document.getElementsByClassName("l1");
+items[2].style.backgroundColor = "green";
+for (var i =0;i<li.length;i++){
+    li[i].style.fontWeight = "bold";}
